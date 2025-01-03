60518d5a42707cfab0eedd7229b7f760a8bd1da8
codetesla <uoladele99@gmail.com>
2025-01-03 13:12:21 +0100

    Refactored code for better performance and readability.

--- src/lib/components/footer.svelte
+++ src/lib/components/footer.svelte
@@ -2,9 +2,9 @@
   const currentYear = new Date().getFullYear();
   
   const socialLinks = [
-    { name: 'Twitter', icon: 'fa-brands fa-x-twitter', href: '#' },
-    { name: 'GitHub', icon: 'fa-brands fa-github', href: '#' },
-    { name: 'LinkedIn', icon: 'fa-brands fa-linkedin', href: '#' }
+    { name: 'Twitter', icon: 'fa-brands fa-x-twitter', href: 'https://x.com/uthman_dev?s=09' },
+    { name: 'GitHub', icon: 'fa-brands fa-github', href: 'https://github.com/codetesla51' },
+    { name: 'LinkedIn', icon: 'fa-brands fa-linkedin', href: 'https://www.linkedin.com/in/oladele-uthman-a61578298' }
   ];
 </script>
 <footer class="bg-black/50 backdrop-blur-lg border-t border-gray-800">
@@ -23,8 +23,7 @@
       </div>
       <!-- Social Links -->
       <div>
-        <h3 class="text-white font-semibold mb-4">Social</h3>
-        <div class="flex flex-col gap-3">
+        <div class="flex  gap-4">
           {#each socialLinks as social}
             <a
               href={social.href}
@@ -49,7 +48,7 @@
           <i class="fas fa-heart text-red-500"></i>
           <span>by</span>
           <a 
-            href="https://github.com/uthmandev" 
+            href="https://github.com/codetesla51" 
             class="text-transparent bg-clip-text bg-gradient-to-r from-blue-400 to-purple-500 hover:opacity-80 transition-opacity duration-200"
           >
             Uthman Dev
